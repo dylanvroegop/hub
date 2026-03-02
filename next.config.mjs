ad2cd4ed647a30d53622d5a0a428950c04aea5b6
Dylan <dylan@yourdomain.com>
2026-03-02 16:22:41 +0200

    chore: optimize next.js output tracing for vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['firebase-admin', '@supabase/supabase-js', 'firebase'],
+  }
+};
 
 export default nextConfig;
